f08b8a3efb1e10c5af8b7389461e7e84b3ba10b5
Чагдонов Гашун Александрович <g.chagdonov@bftcom.com>
2022-03-23 22:42:40 +0300

    запросы GET_MOVIES and GET_MOVIE

--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -1,6 +1,26 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
 
 const Details = () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id },
+  });
+  console.log(id);
+  if (loading) return <div>Загрузка....</div>;
+  if (data?.movie) return <h1>{data.movie.title}</h1>;
   return (
     <div>
       <h1>О Фильме</h1>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,64 @@
+import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
 import React from "react";
+import Movie from "../components/Movie";
+
+const GET_MOVIES = gql`
+  query {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
 
 const Home = () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  console.log(loading, data);
   return (
-    <div>
-      <h1>Главная страница</h1>
-    </div>
+    <Container>
+      <Header>
+        <Title>Apollo 2021</Title>
+        <Subtitle>База фильмов на React, Apollo, GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Загрузка...</Loading>}
+      {!loading && data?.movies && data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+    </Container>
   );
 };
 
