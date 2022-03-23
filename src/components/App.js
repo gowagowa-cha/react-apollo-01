62f640faf370363bfbc576a407f6ccdb7cf55702
Чагдонов Гашун Александрович <g.chagdonov@bftcom.com>
2022-03-23 21:35:17 +0300

    added apollo in react

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../routes/Home";
+import Details from "../routes/Details";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:id" element={<Details />} />
+    </Routes>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import client from "./apollo";
 import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root"),
 );
